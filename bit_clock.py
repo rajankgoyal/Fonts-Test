f50805ac9a84ed13f6dae27c6142fea8fc1ba8a4
rajankgoyal <72119570+rajankgoyal@users.noreply.github.com>
2024-02-05 20:01:17 -0500

    Clock is working now properly.
    Clock is working now properly.

--- bit_clock.py
+++ bit_clock.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, RTC
 from ssd1306 import SSD1306_I2C
 import framebuf,sys, utime
 import one
@@ -14,23 +14,16 @@ pix_res_x  = 128 # SSD1306 horizontal resolution
 pix_res_y = 64   # SSD1306 vertical resolution
 
 i2c_dev = I2C(1,scl=Pin(27),sda=Pin(26),freq=200000)  # start I2C on I2C1 (GPIO 26/27)
-i2c_addr = [hex(ii) for ii in i2c_dev.scan()] # get I2C address in hex format
-if i2c_addr==[]:
-    print('No I2C Display Found') 
-    sys.exit() # exit routine if no dev found
-else:
-    print("I2C Address      : {}".format(i2c_addr[0])) # I2C device address
-    print("I2C Configuration: {}".format(i2c_dev)) # print I2C params
-
-
+rtc=machine.RTC()
 oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev) # oled controller
-
+# TIME
+digit_1,digit_2,digit_3,digit_4 = 0,0,0,0
+# bit_numbers(7)
 oled.fill(0) # clear the OLED
-oled.blit(bit_numbers(1), -5, 19,1) # show the image at location (x=0,y=0)
-oled.blit(bit_numbers(7), 25, 19) # show the image at location (x=0,y=0)
-# oled.blit(fb10, 55, 16) # show the image at location (x=0,y=0)
-oled.blit(bit_numbers(2), 65, 19) # show the image at location (x=0,y=0)
-oled.blit(bit_numbers(7), 95, 19) # show the image at location (x=0,y=0)
+oled.blit(bit_numbers(digit_1), -5, 19) # show the image at location (x=0,y=0)
+oled.blit(bit_numbers(digit_2), 25, 19) # show the image at location (x=0,y=0)
+oled.blit(bit_numbers(digit_3), 65, 19) # show the image at location (x=0,y=0)
+oled.blit(bit_numbers(digit_4), 95, 19) # show the image at location (x=0,y=0)
 ## Calender with weather information
 oled.text('FEB', 0, 2, 1)
 oled.text('14', 29, 2, 1)
@@ -39,6 +32,17 @@ oled.text(' 47', 100, 2, 1)
 oled.show() # show the new text and image
 ## Showing running time with blinking colon indicating seconds
 while True:
+    timestamp=rtc.datetime()
+#     timeString="%02d:%02d:%02d"%(timestamp[4:7])
+    digit_1 = int(timestamp[4]/10)
+    digit_2 = timestamp[4]%10
+    digit_3 = int(timestamp[5]/10)
+    digit_4 = timestamp[5]%10
+    oled.blit(bit_numbers(digit_1), -5, 19) # show the image at location (x=0,y=0)
+    oled.blit(bit_numbers(digit_2), 25, 19) # show the image at location (x=0,y=0)
+    oled.blit(bit_numbers(digit_3), 65, 19) # show the image at location (x=0,y=0)
+    oled.blit(bit_numbers(digit_4), 95, 19) # show the image at location (x=0,y=0)
+    print(digit_4)
     for second in range(2):
         oled.fill_rect(60, 30, 5, 5, second)
         oled.fill_rect(60, 50, 5, 5, second)
